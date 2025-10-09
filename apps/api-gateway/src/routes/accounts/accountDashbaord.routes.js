434670aed815628d409fdb7c8bb0b4e7f093f818
Anwarsadath07 <anwar07sadath@gmail.com>
2025-10-09 11:57:03 +0530

    updates

--- apps/api-gateway/src/routes/accounts/accountDashbaord.routes.js
+++ apps/api-gateway/src/routes/accounts/accountDashbaord.routes.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   getAccountDashboardDataForExpenseSection,
   getAccountDashboardDataForIncomeSection,
+  getGSTReportController,
+
 } from "../../controllers/accounts/dashbaord.controller.js";
 
 const accountDashboardRoutes = express.Router();
@@ -12,4 +14,11 @@ accountDashboardRoutes.get(
   getAccountDashboardDataForExpenseSection
 );
 
+accountDashboardRoutes.get(
+  "/gst-report",
+  getGSTReportController
+);
+
+
 export default accountDashboardRoutes;
+ 
