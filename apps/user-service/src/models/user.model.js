7498ecad55587400bcfb66b74dbeba24b9b58bf5
Akhilbabu456 <akhilbeliever001@gmail.com>
2025-10-15 10:33:06 +0530

    Updates

--- apps/user-service/src/models/user.model.js
+++ apps/user-service/src/models/user.model.js
@@ -274,6 +274,7 @@ const userSchema = new mongoose.Schema(
     gaming: {
       gamePlayed: { type: Boolean, default: false },
       gameCompleted: { type: Boolean, default: false },
+      gameActive: { type: Boolean, default: false },
     },
   },
   {
